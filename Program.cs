948a6d10f0053545c60f439f1eec8ac31050d96b
kristian usifoh <kristianusifoh@outlook.com>
2020-02-13 23:27:19 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//Demonstrate Sockets
+using System;
+using System.Net.Sockets;
+using System.IO;
+public class Whois
+{
+    static void Main(string[] args)
+    {
+
+        TcpClient client = new TcpClient();
+        client.Connect("whois.net.dcs.hull.ac.uk", 43);
+
+        try
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Args should be more than one");
+                return;
+            }
+            else if (args.Length == 1)
+            {
+
+                StreamWriter sw = new StreamWriter(client.GetStream());
+                StreamReader sr = new StreamReader(client.GetStream());
+                sw.WriteLine(args[0]);
+                sw.Flush();
+                Console.WriteLine(args[0] + " is " + sr.ReadToEnd());
+                return;
+            }
+
+
+            else if (args.Length == 2)
+            {
+                StreamWriter sw = new StreamWriter(client.GetStream());
+                StreamReader sr = new StreamReader(client.GetStream());
+                sw.WriteLine(args[0] + " " + args[1]);
+                sw.Flush();
+                Console.WriteLine(args[0] + " location changed to be " + args[1]);
+                sw.Close();
+                return;
+            }
+
+            else
+            {
+                Console.WriteLine("Too many args");
+                return;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+
+        }
+    }
+}
